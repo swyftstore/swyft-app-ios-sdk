64262c14fd9763676d61ca6c6ddc2a8162a782a5
tommanuelDS <tmanuel@dropspotusa.com>
2019-07-05 13:25:36 -0700

    FSA-234: removed isDefault property

--- SwyftSdk/Model/SwyftPaymentMethod.swift
+++ SwyftSdk/Model/SwyftPaymentMethod.swift
@@ -15,7 +15,6 @@ public class SwyftPaymentMethod: FireStoreModelSerialize, FireStoreModelProto  {
     @objc public var last4: String?
     @objc public var cardType: String?
     @objc public var cardExpiry: String?
-    @objc public var isDefault = false
     
     public func toString() {
         //todo
